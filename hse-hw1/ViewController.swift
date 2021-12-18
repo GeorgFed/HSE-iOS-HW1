9232fe30922434ad01fff5f17b47cbf893f2c864
Egor Fedyaev <georgefed@yandex-team.ru>
2021-12-18 17:31:28 +0300

    Initial Commit

---
+++ hse-hw1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hse-hw1
+//
+//  Created by Egor Fedyaev on 18.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
