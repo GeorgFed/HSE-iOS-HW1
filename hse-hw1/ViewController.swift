96b1cace738d0cd546db1d1346b7a3a501a96cdc
Egor Fedyaev <georgefed@yandex-team.ru>
2021-12-20 01:31:44 +0300

    adds ui

--- hse-hw1/ViewController.swift
+++ hse-hw1/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet var views: [UIView]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onChangeColorTap(_ sender: Any) {
+        var colors = Set<UIColor>()
+        while (colors.count < views.count) {
+            colors.insert(UIColor(red: .random(in: 0...1),
+                                  green: .random(in: 0...1),
+                                  blue: .random(in: 0...1),
+                                  alpha: 1))
+        }
+        
+        self.button.isEnabled = false
+        UIView.animate(withDuration: 1, animations: {
+            self.views.forEach { view in
+                view.layer.cornerRadius = .random(in: 0...(view.bounds.height / 2))
+                view.backgroundColor = colors.popFirst()
+                
+            }
+        }) { _ in
+            self.button.isEnabled = true
+        }
+    }
+    
 }
 
